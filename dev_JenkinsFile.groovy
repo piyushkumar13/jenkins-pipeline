3e4c18207def7fc28af2aed6e113890789d27100
Piyush Kumar <piyush.kumar@logmein.com>
2018-04-04 15:11:42 +0530

    Initial commit

---
+++ dev_JenkinsFile.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+    tools {
+        maven 'my-maven'
+        jdk 'my-java'
+    }
+    stages {
+
+        stage('Compile Stage') {
+            steps {
+                sh 'mvn clean compile'
+            }
+        }
+
+        stage('Test Stage') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+
+        stage('Install stage') {
+            steps {
+                sh 'mvn install'
+
+            }
+        }
+        stage('Deploy stage') {
+            steps {
+                sh 'mvn spring-boot:run'
+
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/jenkins/jenkinspipeline/JenkinsPipelineApplication.java
@@ -0,0 +1,12 @@
+package com.example.jenkins.jenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineApplication.class, args);
+	}
+}
