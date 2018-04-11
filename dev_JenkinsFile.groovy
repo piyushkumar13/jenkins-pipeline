bd0370d01f04e2af2f8fbe0d1b6bb262853505cf
Piyush Kumar <piyush.kumar@logmein.com>
2018-04-11 21:14:38 +0530

    commenting deploy step in jenkins file

--- dev_JenkinsFile.groovy
+++ dev_JenkinsFile.groovy
@@ -4,7 +4,39 @@ pipeline {
         maven 'my-maven'
         jdk 'my-java'
     }
+
+    environment {
+        MY_NAME = "Piyush Kumar"
+        MY_ADDRESS = "abc"
+    }
+
+    parameters {
+        string(name: 'jobName', defaultValue: 'my-pipeline-job',description: 'Please enter the jobname')
+    }
+
     stages {
+        stage('Print environment variables') {
+            steps {
+                sh 'echo printenv'
+            }
+        }
+
+        stage('Printing predefined environment variables') {
+
+            steps {
+                sh '''
+                            echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL} \n"
+                            echo "This is the example of echoing predefined environment variables."
+
+                          '''
+            }
+        }
+
+        stage('Printing Parameter values'){
+            steps {
+                sh 'echo "The name of the job is ${params.jobName}'
+            }
+        }
 
         stage('Compile Stage') {
             steps {
@@ -19,6 +51,13 @@ pipeline {
         }
 
         stage('Install stage') {
+            input {
+                message "Should we continue?"
+                ok "Yes, we should."
+                parameters {
+                    string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+                }
+            }
             steps {
                 sh 'mvn install'
 
@@ -30,5 +69,8 @@ pipeline {
 //
 //            }
 //        }
+    }
+    post {
+        echo 'The pipeline has finish!'
     }
 }
