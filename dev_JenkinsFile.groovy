f234cbc237d80bd728ec55e5d48b96eaee9e1a48
Piyush Kumar <piyush.kumar@logmein.com>
2018-05-02 22:36:11 +0530

    adding environment variable to the input

--- dev_JenkinsFile.groovy
+++ dev_JenkinsFile.groovy
@@ -36,20 +36,11 @@ pipeline {
                     message 'Let\'s promote?'
                     parameters {
 
-//                        string(name: 'PERSON', defaultValue: MY_NAME, description: 'Who should I say hello to?')
-//                        string(name: 'Last', defaultValue: 'kumar', description: 'Who should I say hello to?')
+                        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+                        string(name: 'Last', defaultValue: 'kumar', description: 'Who should I say hello to?')
 
-                        $class: 'GlobalVariableStringParameterDefinition'(defaultValue: 'Piyush', description: 'Your name', name: 'pname')
-                        $class: 'GlobalVariableStringParameterDefinition'(defaultValue: 'Kumar', description: 'Your Last name', name: 'lname')
-
-//                        [
-//
-//                                [$class: 'TextParameterDefinition', defaultValue: 'valueone', description: 'one', name: 'valueone'],
-//                                [$class: 'TextParameterDefinition', defaultValue: 'valuetwo', description: 'two', name: 'valuetwo'],
-//                                [$class: 'TextParameterDefinition', defaultValue: 'valuethree', description: 'three', name: 'valuethree'],
-//                                [$class: 'TextParameterDefinition', defaultValue: 'valuefour', description: 'fouter', name: 'valuefour']
-//
-//                        ]
+//                        [$class: 'GlobalVariableStringParameterDefinition',defaultValue: 'Piyush', description: 'Your name', name: 'pname']
+//                        [$class: 'GlobalVariableStringParameterDefinition',defaultValue: 'Kumar', description: 'Your Last name', name: 'lname']
 
                     }
 
